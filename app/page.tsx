2e042532ed8e0f9e86943583edbfb7dfebc48902
maruf21hossain <marufhossainmunna21@gmail.com>
2024-02-03 15:04:27 +0600

    Testimonial problem fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import ConsultationBookung from "@/components/common/ConsultationBookung";
 import Faqs from "@/components/common/Faqs";
 import LetsTalk from "@/components/common/LetsTalk";
 import HeroArea from "@/components/home/HeroArea";
+import Slider from "@/components/home/TextimonialCard";
 import Testimonials from "@/components/home/Testimonials";
 import Trust from "@/components/home/Trust";
 import DigitalSoluations from "@/components/home/digitalSoluations/DigitalSoluations";
--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -1,32 +1,10 @@
 import React from 'react';
 import Container from '../common/Container';
 import Heading from '../common/heading/Heading';
-import TestimonialCard from './TestimonialCard';
+import TestimonialCard from './TextimonialCard';
 
 const Testimonials = () => {
-    const testimonials = [
-        {
-            id : 1,
-            comment : "Miranda and Edward are absolutely wonderful at what they do. I first came to Miranda earlier this year with a business plan, but little to no idea about how to create/cultivate an online presence. Miranda and Edward have turned my “idea” into a steady, growing business that continues to perform better each month. I love their creativity, go-getter attitudes, and dedication to providing a solid product. Can’t recommend them enough.",
-            userName : "-Jessica kaur",
-            userTitle : "Naina and Noor",
-            userProfile : "/images/profileImg1.png"
-        },
-        {
-            id : 2,
-            comment : "Miranda and Edward are absolutely wonderful at what they do. I first came to Miranda earlier this year with a business plan, but little to no idea about how to create/cultivate an online presence. Miranda and Edward have turned my “idea” into a steady, growing business that continues to perform better each month. I love their creativity, go-getter attitudes, and dedication to providing a solid product. Can’t recommend them enough.",
-            userName : "-James Bond",
-            userTitle : "Naina and Noor",
-            userProfile : "/images/profileImg1.png"
-        },
-        {
-            id : 3,
-            comment : "Miranda and Edward are absolutely wonderful at what they do. I first came to Miranda earlier this year with a business plan, but little to no idea about how to create/cultivate an online presence. Miranda and Edward have turned my “idea” into a steady, growing business that continues to perform better each month. I love their creativity, go-getter attitudes, and dedication to providing a solid product. Can’t recommend them enough.",
-            userName : "-Alex Henry",
-            userTitle : "Naina and Noor",
-            userProfile : "/images/profileImg1.png"
-        },
-    ]
+    
     return (
         <div className='my-16'>
             <Container>
@@ -42,12 +20,6 @@ const Testimonials = () => {
                 <div>
                     <TestimonialCard />
                 </div>
-
-                {/* <div>
-                    {
-                        testimonials.map((testimonial) => <TestimonialCard key={testimonial.id}  />)
-                    }
-                </div> */}
             </Container>
         </div>
     );

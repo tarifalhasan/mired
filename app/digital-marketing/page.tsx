90f53eb35d02e8d4395b547ebd984c7ea232ad6a
tarifalhasan <tarifalhasan999@gmail.com>
2024-02-07 20:34:29 +0600

    MarketingHeroArea

--- app/digital-marketing/page.tsx
+++ app/digital-marketing/page.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import MarketingHeroArea from './MarketingHeroArea';
-import EnsureYourBrand from './EnsureYourBrand';
-import LetsTalk from '@/components/common/LetsTalk';
-import Faqs from '@/components/common/Faqs';
-import BookingConsultation from '@/components/common/BookingConsultation';
-import WeFocus from './WeFocus';
+import BookingConsultation from "@/components/common/BookingConsultation";
+import Faqs from "@/components/common/Faqs";
+import LetsTalk from "@/components/common/LetsTalk";
+import EnsureYourBrand from "./EnsureYourBrand";
+import MarketingHeroArea from "./MarketingHeroArea";
+import WeFocus from "./WeFocus";
 
 const page = () => {
-    return (
-        <div>
-            <MarketingHeroArea />
-            <EnsureYourBrand />
-            <WeFocus />
-            <BookingConsultation />
-            <Faqs />
-            <LetsTalk />
-        </div>
-    );
+  return (
+    <div>
+      <MarketingHeroArea />
+      <EnsureYourBrand />
+      <WeFocus />
+      <BookingConsultation />
+      <Faqs />
+      <LetsTalk />
+    </div>
+  );
 };
 
 export default page;
